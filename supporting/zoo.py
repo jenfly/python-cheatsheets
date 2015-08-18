42f29479b5dac75bfdba81fad00253e656da07b5
Jennifer Walker <jenfly@gmail.com>
2015-08-18 16:55:43 -0700

    Deciding on sub-directory names

---
+++ supporting/zoo.py
@@ -0,0 +1,8 @@
+def visit(animals, name):
+    print('Welcome to the zoo, ' + name + '! We have: ')
+    for key in animals: 
+        print('  %d %s' % (animals[key], key))
+
+animals = {'zebras': 5, 'elephants': 4, 'penguins': 10}
+name = 'Jennifer'
+visit(animals, name)
