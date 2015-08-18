9d5b32e6d5619b3ccea5cb97e625dfb8c0f5cc0d
Jennifer Walker <jenfly@gmail.com>
2015-08-18 16:52:52 -0700

    More reorganizing

--- basics/zoo.py
+++
@@ -1,8 +0,0 @@
-def visit(animals, name):
-    print('Welcome to the zoo, ' + name + '! We have: ')
-    for key in animals: 
-        print('  %d %s' % (animals[key], key))
-
-animals = {'zebras': 5, 'elephants': 4, 'penguins': 10}
-name = 'Jennifer'
-visit(animals, name)
