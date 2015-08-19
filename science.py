468494a00cdf671ca12336d196aba2bba2fe76aa
Jennifer Walker <jenfly@gmail.com>
2015-08-18 17:33:05 -0700

    NetCDF file reading and simple basemap plotting

--- science.py
+++ science.py
@@ -4,3 +4,22 @@
 - matplotlib
 - scipy
 '''
+
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.basemap import Basemap
+import netCDF4 as nc
+
+filename = 'data/MERRA.201401.SUB.nc'
+fh = Dataset(filename, mode='r')
+lon = fh.variables['longitude'][:]
+lat = fh.variables['latitude'][:]
+ps = fh.variables['ps'][:]
+print(lon.shape)
+print(lat.shape)
+print(ps.shape)
+fh.close()
+
+m = Basemap()
+m.drawcoastlines()
+m.drawcountries()
