1069310dbdd27ac97e1628609fc9574a05efe225
Jennifer Walker <jenfly@gmail.com>
2015-08-27 13:25:33 -0700

    Added example Vector class

--- advanced.py
+++ advanced.py
@@ -1,6 +1,110 @@
 '''
-- Fancier file I/O
-- Errors and exceptions
+Jennifer's cheatsheet for advanced Python topics.
 - Classes
-- sys module
+
+Each section of this cheatsheet can be copy/pasted into ipython (using the
+%paste magic command for indented code) and run separately in an interactive
+session.
+
+Many of these code snippets are pilfered / adapted from:
+- https://docs.python.org/2/tutorial/
+- http://www.tutorialspoint.com/python/python_classes_objects.htm
+
 '''
+
+import math
+
+print("\nWelcome to Jennifer's advanced Python cheatsheet!")
+
+def heading(s):
+    '''Prints a nice heading to the console.'''
+    line = '-' * 40
+    print('\n' + line + '\n' + s + '\n' + line)
+
+# ----------------------------------------------------------------------
+# Classes
+# ----------------------------------------------------------------------
+
+heading('Classes')
+
+# Defining a class
+class Vector:
+    '''Vector in x,y plane.'''
+
+    def __init__(self, x, y):
+        '''Initializes instance object with a call like v = Vector(x, y)'''
+        self.x = float(x)
+        self.y = float(y)
+
+    def __str__(self):
+        '''Returns a printable string representation of the instance object'''
+        return 'Vector (%d, %d)' % (self.x, self.y)
+
+    def __add__(self, other):
+        '''Overloads the + operator to perform vector addition'''
+        return Vector(self.x + other.x, self.y + other.y)
+
+    def length(self):
+        '''Returns the length of the vector measured from the origin'''
+        return math.sqrt(self.x**2 + self.y**2)
+
+    def angle(self, degrees=True):
+        '''
+        Returns the angle of the vector from the x-axis.
+
+        Default units are degrees. If argument degrees=False, returns the
+        angle in radians.
+        '''
+        theta = math.atan(self.y / self.x)
+        if self.x < 0:
+            theta += math.pi
+        if degrees:
+            theta = math.degrees(theta)
+        return theta
+
+    def distance(self, other):
+        '''Returns the distance between the endpoints of two vectors.'''
+        return math.sqrt( (other.x - self.x) ** 2 + (other.y - self.y) ** 2)
+
+    def disp(self):
+        '''Prints detailed information about the vector'''
+        print(self)
+        print('  Length: %f' % self.length())
+        print('  Angle: %f degrees' % self.angle())
+
+
+# Using the class
+v1 = Vector(2, 10)
+print(v1)
+
+# Calling v1.length() is equivalent to Vector.length(v1)
+print(v1.length())
+print(v1.angle(degrees=False))
+v1.disp()
+
+v2 = Vector(-5, 2)
+print(v1 + v2)
+print(v1.distance(v2))
+
+# Making a standalone function with vector objects as input
+def vdistance(v1, v2):
+    '''Distance between endpoints of two vectors (objects of class Vector)'''
+    return math.sqrt( (v2.x - v1.x) ** 2 + (v2.y - v1.y) ** 2)
+
+# vdistance(v1, v2) is now equivalent to v1.distance(v2)
+print(vdistance(v1, v2))    
+
+
+# ----------------------------------------------------------------------
+# Fancier file I/O
+# ----------------------------------------------------------------------
+
+
+# ----------------------------------------------------------------------
+# Errors and exceptions
+# ----------------------------------------------------------------------
+
+
+# ----------------------------------------------------------------------
+# sys module
+# ----------------------------------------------------------------------
