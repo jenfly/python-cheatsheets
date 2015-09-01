cd0e9bc8b708bc49e5ccc1edc0a2b4da0125cabd
Jennifer Walker <jenfly@gmail.com>
2015-08-31 19:39:54 -0700

    Added style.py

---
+++ style.py
@@ -0,0 +1,17 @@
+"""
+Styles and conventions I follow to keep my code consistent.
+"""
+
+# ----------------------------------------------------------------------
+# Highlights from PEP0008 Style Guide for Python
+# ----------------------------------------------------------------------
+
+print("""
+PEP8 Highlights
+- 4 spaces per indent
+- Max line length: 79 characters (72 for docstrings)
+- 2 blank lines around top-level function and class definitions
+- 1 blank line around method definitions within a class
+""")
+
+# Continuation lines - 2 options
