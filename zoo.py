3bd342090876549093c5fe41c77c90463b574f55
Jennifer Walker <jenfly@gmail.com>
2015-09-01 10:40:05 -0700

    Update zoo.py

--- zoo.py
+++ zoo.py
@@ -1,6 +1,6 @@
 def visit(animals, name):
     print('Welcome to the zoo, ' + name + '! We have: ')
-    for key in animals: 
+    for key in animals:
         print('  %d %s' % (animals[key], key))
 
 animals = {'zebras': 5, 'elephants': 4, 'penguins': 10}
