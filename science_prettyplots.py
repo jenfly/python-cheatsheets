5d451d22414f2eead1f34a85dfbece73840eee03
Jennifer Walker <jenfly@gmail.com>
2015-11-01 08:02:15 -0800

    Added examples with subplots and sharing axes

--- science_prettyplots.py
+++ science_prettyplots.py
@@ -55,6 +55,66 @@ heading('Making plots pretty')
 # ----------------------------------------------------------------------
 
 # ----------------------------------------------------------------------
+# Subplots
+# ----------------------------------------------------------------------
+
+# Simple data to display in various forms
+x = np.linspace(0, 2 * np.pi, 400)
+y = np.sin(x ** 2)
+
+# Two subplots, the axes array is 1-d
+f, axarr = plt.subplots(2, sharex=True)
+axarr[0].plot(x, y)
+axarr[0].set_title('Sharing X axis')
+axarr[1].scatter(x, y)
+
+# Two subplots, unpack the axes array immediately
+f, (ax1, ax2) = plt.subplots(1, 2, sharey=True)
+ax1.plot(x, y)
+ax1.set_title('Sharing Y axis')
+ax2.scatter(x, y)
+
+# Three subplots sharing both x/y axes
+f, (ax1, ax2, ax3) = plt.subplots(3, sharex=True, sharey=True)
+ax1.plot(x, y)
+ax1.set_title('Sharing both axes')
+ax2.scatter(x, y)
+ax3.scatter(x, 2 * y ** 2 - 1, color='r')
+# Fine-tune figure; make subplots close to each other and hide x ticks for
+# all but bottom plot.
+f.subplots_adjust(hspace=0)
+plt.setp([a.get_xticklabels() for a in f.axes[:-1]], visible=False)
+
+# row and column sharing
+f, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, sharex='col', sharey='row')
+ax1.plot(x, y)
+ax1.set_title('Sharing x per column, y per row')
+ax2.scatter(x, y)
+ax3.scatter(x, 2 * y ** 2 - 1, color='r')
+ax4.plot(x, 2 * y ** 2 - 1, color='r')
+
+# Four axes, returned as a 2-d array
+f, axarr = plt.subplots(2, 2)
+axarr[0, 0].plot(x, y)
+axarr[0, 0].set_title('Axis [0,0]')
+axarr[0, 1].scatter(x, y)
+axarr[0, 1].set_title('Axis [0,1]')
+axarr[1, 0].plot(x, y ** 2)
+axarr[1, 0].set_title('Axis [1,0]')
+axarr[1, 1].scatter(x, y ** 2)
+axarr[1, 1].set_title('Axis [1,1]')
+# Fine-tune figure; hide x ticks for top plots and y ticks for right plots
+plt.setp([a.get_xticklabels() for a in axarr[0, :]], visible=False)
+plt.setp([a.get_yticklabels() for a in axarr[:, 1]], visible=False)
+
+# Sharing x, y axes, adjusting spacing between subplots
+fig, axes = plt.subplots(2, 2, sharex=True, sharey=True)
+for i in range(2):
+    for j in range(2):
+        axes[i,j].hist(np.random.randn(500), bins=50, color='k', alpha=0.5)
+plt.subplots_adjust(wspace=0.2, hspace=0)
+
+# ----------------------------------------------------------------------
 # Formatting examples
 # ----------------------------------------------------------------------
 
@@ -68,12 +128,6 @@ plt.rc('font', **font_options)
 # matplotlib/mpl-data/matplotlibrc -- customize and save as
 # ~/.matplotlibrc to load customized defaults each time you use matplotlib
 
-# Sharing x, y axes, adjusting spacing between subplots
-fig, axes = plt.subplots(2, 2, sharex=True, sharey=True)
-for i in range(2):
-    for j in range(2):
-        axes[i,j].hist(np.random.randn(500), bins=50, color='k', alpha=0.5)
-plt.subplots_adjust(wspace=0, hspace=0)
 
 
 # pd.scatter_matrix() example from pydata-book
